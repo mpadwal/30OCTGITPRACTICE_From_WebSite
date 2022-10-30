562caa765961f3baa3f9aacd4852b82ae19cec25
Mayuri <mayuripadwal1191@gmail.com>
2022-10-30 15:56:13 +0530

    test

---
+++ src/main/java/Pages_Packages/Home_Page.java
@@ -0,0 +1,13 @@
+package Pages_Packages;
+
+import org.testng.annotations.Test;
+
+public class Home_Page
+{
+	@Test
+	public static void test()
+	{
+		System.out.println("This is Home Page Test");
+	}
+	
+}
---
+++ src/test/java/Test_Cases/Home_Page_testcase.java
@@ -0,0 +1,14 @@
+package Test_Cases;
+
+import org.testng.annotations.Test;
+
+public class Home_Page_testcase 
+{
+
+	@Test
+	public static void test()
+	{
+		System.out.println("This is Home Page Test");
+	}
+	
+}
